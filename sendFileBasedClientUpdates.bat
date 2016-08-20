c03b0c54574588524fb2f4615b744d73e734dd35
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2016-08-20 19:19:10 +0200

    QA-36: insi is now a fully fledged command line app + Jentsch
    Change-Id: Id62c46ec430b50ba87c3b935a018ca919771673e

--- sendFileBasedClientUpdates.bat
+++
@@ -1,2 +0,0 @@
-set JAVA_OPTS=-DurlPrefix=http://localhost:9000/v2 -DnumClients=1 -DsendDelay=1 -DbatchSize=100 -DfileName=../346181868.gpx
-sbt "gatling:testOnly SendFileBasedClientUpdates"
--- sendRandomClientUpdates.bat
+++
@@ -1,2 +0,0 @@
-set JAVA_OPTS=-DurlPrefix=http://localhost:9000/v2 -DnumClients=1 -DsendDelay=1 -DbatchSize=100
-sbt "gatling:testOnly SendRandomClientUpdates"
